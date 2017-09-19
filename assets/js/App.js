523d95de30a7f3e6283e4e0336e1821ed61288ed
donaldvallejo <donny.vallejo@gmail.com>
2017-09-19 15:43:21 -0700

    getting closer

--- assets/js/App.js
+++ assets/js/App.js
@@ -15,33 +15,39 @@ $(document).on("ready", function(){
       $(".gif-buttons").show().prepend("<button id='gifButton'>" + buttonText + "</button>");
     }
     makeButton();
-  });
+
+    function showgif() {
+      $.ajax({
+        method: "GET",
+        url: giphy_api,
+        data: $("form").serialize(),
+        success: onSuccess,
+        error: onError
+      });
+    }
+
+    function onSuccess(json) {
+      if (json.pagination.offset === 0) {
+        $(".gif-img").remove();
+      }
+      json.data.forEach(function(v){
+        $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
+      });
+    }
+
+    function onError(xhr, status, errorThrown) {
+      alert("I dont know, what happened!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    }
+
+    $("#gifButton").click(function(){
+      console.log("im clicked")
+      $(".gif-buttons").showgif();
+    })
+    // showgif();
+});
 
 
 });
-
-function showgif() {
-  $.ajax({
-    method: "GET",
-    url: giphy_api,
-    data: $("form").serialize(),
-    success: onSuccess,
-    error: onError
-  });
-}
-
-function onSuccess(json) {
-  if (json.pagination.offset === 0) {
-    $(".gif-img").remove();
-  }
-  json.data.forEach(function(v){
-    $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
-  });
-}
-
-function onError(xhr, status, errorThrown) {
-  alert("I dont know, what happened!");
-  console.log("Error: " + errorThrown);
-  console.log("Status: " + status);
-  console.dir(xhr);
-}
