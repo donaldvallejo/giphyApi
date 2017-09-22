e6376c02bbc4d3a78f8ec7323721c9116a078573
donaldvallejo <donny.vallejo@gmail.com>
2017-09-22 16:44:31 -0700

    done

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,98 +1,70 @@
 var giphy_api = "http://api.giphy.com/v1/gifs/search";
 
 $(document).on("ready", function(){
+  // logic function for previous search buttons to populate element with gifs
 
-  var input = $(this).attr("data-input");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-  input + "&api_key=dc6zaTOxFJmzC&limit=20";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  })
-  // After data comes back from the request
-  .done(function(response) {
-    console.log(queryURL);
+  $("#go").on("click", function(e) {
+    e.preventDefault();
+    var searchString = $("#buttonText").val();
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchString + "&api_key=dc6zaTOxFJmzC&limit=5";
 
-    console.log(response);
-    // storing the data from the AJAX request in the results variable
-    var results = response.data;
-
-    // Looping through each result item
-    for (var i = 0; i < results.length; i++) {
-      console.log("looping");
-      // Creating and storing a div tag
-      var inputDiv = $("<div>");
-
-      // Creating a paragraph tag with the result item's rating
-      var p = $("<p>").text("Rating: " + results[i].rating);
-
-      // Creating and storing an image tag
-      var inputImage = $("<img>");
-      // Setting the src attribute of the image to a property pulled off the result item
-      inputImage.attr("src", results[i].images.fixed_height.url);
-
-      // Appending the paragraph and image tag to the inputDiv
-      inputDiv.append(p);
-      inputDiv.append(inputImage);
-
-      // Prependng the inputDiv to the HTML page in the "#gifs-appear-here" div
-      $(".gif-gallery").prepend(inputDiv);
-
-    }
-
-    $("form").on("submit", function(e) {
-      e.preventDefault();
-
-      $("form [name='offset']").val(0);
-      // showgif();
-
-      var buttonText = $("#buttonText").val();
-
-      function makeButton() {
-        $(".gif-buttons").show().prepend("<button id='gifButton'>" + buttonText + "</button>");
-      }
-      makeButton();
-
-      // logic function for previous serch buttons to populate element with gifs
-
-      $("#gifButton").click(function(event){
-        var url = $(this).attr('src');
+    function makeButton() {
+      $(".gif-buttons").append("<button class='gifButton' id=" + searchString + ">" + searchString + "</button>");
+      $("#" + searchString).click(function(){
         console.log("im clicked");
+        fetchAndDisplayGifs();
+      });
+    }
+    makeButton();
 
+    // make last elements empty() when a new search is clicked
+
+    //  connect #buttonText value to the gif loop to print them on the page.
+    function fetchAndDisplayGifs() {
+      console.log("queryURL", queryURL);
+      console.log("searchString", searchString );
+      $.ajax({
+        url: queryURL,
+        method: "GET",
       })
-  });
+      // After data comes back from the request
+      .done(function(response) {
 
+        // if there are gifs on the page
+        if ($(".newGifs").length > 0) {
+          // remove gifs from page
+          console.log("removing");
+          $(".newGifs").remove();
+        }
 
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+        console.log("results", results);
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+          console.log("looping");
+          // Creating and storing a div tag
+          var inputDiv = $("<div>").addClass("newGifs");
 
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
 
+          // Creating and storing an image tag
+          var inputImage = $("<img>");
+          var gifURL = results[i].images.fixed_height.url
+          // Setting the src attribute of the image to a property pulled off the result item
+          inputImage.attr("src", gifURL);
 
+          // Appending the paragraph and image tag to the inputDiv
+          inputDiv.append(p);
+          inputDiv.append(inputImage);
 
-    // function showgif() {
-    //   $.ajax({
-    //     method: "GET",
-    //     url: giphy_api,
-    //     data: $("form").serialize(),
-    //     success: onSuccess,
-    //     error: onError
-    //   });
-    // }
-    //
-    // function onSuccess(json) {
-    //   if (json.pagination.offset === 0) {
-    //     $(".gif-img").remove();
-    //   }
-    //   json.data.forEach(function(k, v){
-    //     $(".gif-gallery").append($("<img class='img-responsive img-thumbnail gif-img' src="+v.images.fixed_height.url+">"));
-    //   });
-    // }
-    //
-    // function onError(xhr, status, errorThrown) {
-    //   alert("I dont know, what happened!");
-    //   console.log("Error: " + errorThrown);
-    //   console.log("Status: " + status);
-    //   console.dir(xhr);
-    // }
-
+          // Prependng the inputDiv to the HTML page in the "#gifs-appear-here" div
+          $(".gif-gallery").prepend(inputDiv);
+        }
+      });
+    }
+    fetchAndDisplayGifs();
   });
 });
